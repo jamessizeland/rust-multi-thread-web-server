3442165d4ef9db32d9108622c3580a5c54ae9c6b
James Sizeland <jamessizeland@gmail.com>
2022-05-21 09:27:19 +0100

    add http get response reader to main

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,28 @@
-use std::net::TcpListener;
+use std::io::prelude::*; // We bring into scope to get access to certain traits that let us read from and write to the stream
+use std::net::{TcpListener, TcpStream};
 
+/// Simple TCP Web Service
+///
+/// nonadministrators can listen only on ports higher than 1023
+///
+/// <https://doc.rust-lang.org/book/ch20-01-single-threaded.html>
+///
+/// Because we’re writing a basic server just for learning purposes, we won’t worry about handling these kinds of errors; instead, we use unwrap to stop the program if errors happen.
 fn main() {
     println!("Hello, from my web service");
-
-    // https://doc.rust-lang.org/book/ch20-01-single-threaded.html
     let listener = TcpListener::bind("localhost:7878").unwrap();
-
+    // A single stream represents an open connection between the client and the server
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-
-        println!("Connection Established!!");
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024]; // 1024 bytes in size
+    println!("Connection Established!!");
+
+    stream.read(&mut buffer).unwrap();
+    println!("Request {}", String::from_utf8_lossy(&buffer[..]));
+    // The “lossy” part of the name indicates the behavior of this function when it sees an invalid UTF-8 sequence
 }
